88d3510c0264b720b8a74b1aed8099652a2f4e68
Ehsan Sadeghi <sadeghii.ehsan@gmail.com>
2023-01-08 00:25:06 +0330

    fonts, icons, color plate, prettier added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="flex min-h-screen w-screen items-center justify-center text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="flex min-h-screen w-screen items-center justify-center text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#009688",
+        accent: "#94C720",
+        warn: "#C70D38",
+        text: "#404040",
+        "table-header": "#8d8d8d"
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
